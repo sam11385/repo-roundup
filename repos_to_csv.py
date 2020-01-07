f391cc8e15594a6da2337d0932f81568c461fa30
sam11385 <samluedke@gmail.com>
2020-01-07 10:07:31 -0700

    Making a few updates to the base script.

--- repos_to_csv.py
+++ repos_to_csv.py
@@ -1,32 +1,33 @@
 import csv
 import requests
+import os
 
 def get_repo_list():
 
     # change these things for your org
-    org_name = 'datamade'
-    pages_to_fetch = 2 # number of repos you have, divided by 100. kinda hack-y!
+    # org_name = 'ORG NAME'
+    pages_to_fetch = 3 # number of repos you have, divided by 100. kinda hack-y!
 
-    api_url = 'https://api.github.com/orgs/%s/repos?per_page=100' % org_name
+    api_url = 'https://api.github.com/orgs/%s/repos?per_page=300' % org_name
     repos_list = []
 
     fields = ['name',
+              'archived',
               'html_url',
-              'stargazers_count',
-              'forks',
               'description',
-              'homepage',
               'language',
               'created_at',
               'updated_at']
 
     for page in range(1,(pages_to_fetch + 1)):
         print("Page %s" % page)
-        repos = requests.get(api_url + "&page=" + str(page))
-        if repos.status_code is 200:
+        headers = {"Authorization": "token {0}".format(os.environ['GITHUB_ACCESS_TOKEN']),
+        "Accept": "application/vnd.github.nebula-preview+json"}
+        repos = requests.get(api_url + "&page=" + str(page), headers=headers)
+        if repos.status_code == 200:
             for r in repos.json():
                 repos_list.append([r[key] for key in fields])
-
+    print(len(os.environ['GITHUB_ACCESS_TOKEN']))
     print("Repo list created. Length: %s" % len(repos_list))
 
     outp = open(('%s_repos.csv' % org_name), 'w')
